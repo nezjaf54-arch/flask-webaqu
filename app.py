5b15afdd7e2fce6ec89fb138f131a1018d6fb2f9
nezjaf54-arch <nezjaf54@gmail.com>
2025-11-24 11:18:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-REBUILD_TRIGGER = "v1.0"
+REBUILD_TRIGGER = "v1.4"
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import sys
@@ -47,5 +47,6 @@ def pesan():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
+
 
 
